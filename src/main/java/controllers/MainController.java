ad2fd41323b5814a5eb897cc381092f45b0bb288
NoFearlys <trofimov_a_a@mail.ru>
2021-10-10 16:14:10 +0300

    test4/mapping trouble

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+        public String index(){
+        return "result";
+    }
+    @GetMapping("/calc")
+    public String calc(@RequestParam(value = "firstNumber",required = false) int firstNumber, @RequestParam(value = "secondNumber", required = false) int secondNumber, @RequestParam(value = "operation", required = false) String operation){
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(operation);
+        return "result";
+    }
+}
